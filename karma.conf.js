cd67c5c41e0585b3d97a04ec13908ef79887a4b9
Marco Studerus <mail@mstuderus.ch>
2017-03-07 20:43:12 +0100

    coveralls test exclude tests

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function(config) {
           // source files, that you wanna generate coverage for
           // do not include tests or libraries
           // (these files will be instrumented by Istanbul)
-          'components/**/*.js': ['coverage'],
+          'components/**/!(_test).js': ['coverage'],
           'view*/**/*.js': ['coverage'],
           '*.js': ['coverage']
 
