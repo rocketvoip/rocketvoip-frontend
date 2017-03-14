20cf41510b9dec2fff6b59c6c90a147b8494f58d
Marco Studerus <mail@mstuderus.ch>
2017-03-14 08:00:04 +0100

    Reformat Code

--- app/components/version/version_test.js
+++ app/components/version/version_test.js
@@ -1,11 +1,11 @@
 'use strict';
 
-describe('rocketvoip.version module', function() {
-  beforeEach(module('rocketvoip.version'));
+describe('rocketvoip.version module', function () {
+    beforeEach(module('rocketvoip.version'));
 
-  describe('version service', function() {
-    it('should return current version', inject(function(version) {
-      expect(version).toEqual('0.1');
-    }));
-  });
+    describe('version service', function () {
+        it('should return current version', inject(function (version) {
+            expect(version).toEqual('0.1');
+        }));
+    });
 });
--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -2,41 +2,36 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
-describe('rocket voip', function() {
+describe('rocket voip', function () {
 
 
-  it('should automatically redirect to /dashboard when location hash/fragment is empty', function() {
-    browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/dashboard");
-  });
-
-
-  describe('dashboard', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#!/dashboard');
+    it('should automatically redirect to /dashboard when location hash/fragment is empty', function () {
+        browser.get('index.html');
+        expect(browser.getLocationAbsUrl()).toMatch("/dashboard");
     });
 
 
-    it('should render dashboard when user navigates to /dashboard', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/version of the dashboard/);
+    describe('dashboard', function () {
+
+        beforeEach(function () {
+            browser.get('index.html#!/dashboard');
+        });
+
+        it('should render dashboard when user navigates to /dashboard', function () {
+            expect(element.all(by.css('[ng-view] p')).first().getText()).toMatch(/version of the dashboard/);
+        });
+
     });
 
-  });
+    describe('view2', function () {
 
+        beforeEach(function () {
+            browser.get('index.html#!/view2');
+        });
 
-  describe('view2', function() {
+        it('should render view2 when user navigates to /view2', function () {
+            expect(element.all(by.css('[ng-view] p')).first().getText()).toMatch(/partial for view 2/);
+        });
 
-    beforeEach(function() {
-      browser.get('index.html#!/view2');
     });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
-
-  });
 });
