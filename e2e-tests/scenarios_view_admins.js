2dd7eea0d6543fa640de86a0718a5dc98f4e4927
Marco Studerus <mail@mstuderus.ch>
2017-05-09 14:24:06 +0200

    E2E test for view_admins

---
+++ e2e-tests/scenarios_view_admins.js
@@ -0,0 +1,43 @@
+'use strict';
+
+describe('rocketvoip', function () {
+
+    describe('view_users', function () {
+
+        beforeEach(function () {
+            browser.get('index.html');
+
+            var mockFunction = function () {
+                angular.module('httpBackendMock', ['ngMockE2E'])
+                    .run(function ($httpBackend) {
+                        $httpBackend.whenGET(/\/admins/).respond(function () {
+                            return [200, [], {}];
+                        });
+                        $httpBackend.whenPOST(/login/).respond(function () {
+                            return [200, {}, {'X-Auth-Token': 'Test-Token'}];
+                        });
+                        $httpBackend.whenGET(/.*/).passThrough();
+                    });
+            };
+            browser.addMockModule('httpBackendMock', mockFunction);
+
+            browser.driver.isElementPresent(by.id('username')).then(function (isPresent) {
+                if (isPresent) {
+                    element(by.id('username')).clear().sendKeys("test@test.ch");
+                    element(by.id('password')).clear().sendKeys("password");
+                    element(by.id('viewLoginForm-login')).click();
+                }
+            });
+            browser.get('index.html#!/view_admins');
+        });
+
+        it('should show menu button', function () {
+            expect(element(by.css('a[ng-href="#!/view_admins"]')).isPresent()).toBeTruthy();
+        });
+
+        it('should diaplay site', function () {
+            expect(element(by.id('view_admin_toolbar')).isPresent()).toBeTruthy();
+        });
+
+    });
+});
