24e7735338689542f5d67caf54cdef2518713631
Marco Studerus <mail@mstuderus.ch>
2017-05-16 12:37:14 +0200

    Change AdminPasswordService URL

--- app/view_admins/dialog_resetPassword.js
+++ app/view_admins/dialog_resetPassword.js
@@ -5,18 +5,24 @@
  */
 angular.module('rocketvoip.dialog_resetPassword', ['ngMessages','ngPassword'])
     .controller('ResetPasswordDialogCtrl',
-        function ($scope, $mdDialog, admin, AdminService) {
+        function ($scope, $mdDialog, admin, AdminPasswordService) {
             $scope.admin = angular.copy(admin);
             $scope.admin.password = "";
             $scope.confirmPassword = "";
             
             $scope.save = function () {
                 if ($scope.resetPasswordForm.$valid){
-                    AdminService.update($scope.admin);
+                    AdminPasswordService.update($scope.admin);
                     $scope.close();
                 }
             };
             $scope.close = function () {
                 $mdDialog.cancel();
             };
-        });
+        }).factory('AdminPasswordService', ['$resource', 'appConfig', function ($resource, appConfig) {
+    return $resource(appConfig.BACKEND_BASE_URL + appConfig.API_ENDPOINT + '/admins/:id/password', {id: "@id"}, {
+        update: {
+            method: 'PUT'
+        }
+    });
+}]);
